6bbc177eedfb374c20167c0c3877aca2fe0f78ee
Shaurya Sharma <shaurya.s.hp@gmail.com>
2026-04-06 10:20:35 +0530

    Add files via upload
    Printed Hello world using for each loop

---
+++ HelloApp/src/main/java/com/helloapp/HelloApp.java
@@ -0,0 +1,23 @@
+package com.helloapp;
+
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        StringBuilder names = new StringBuilder();
+
+        for (int i = 0; i < args.length; i++) {
+            names.append(args[i]);
+
+            if (i < args.length - 1) {
+                names.append(", ");
+            }
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
